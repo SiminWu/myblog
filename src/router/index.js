67e73c7fec289f81a7e86a66cb6438d0d56bd19e
-g <wsm2@xlbzone.com>
2017-12-12 18:39:44 +0800

    环境配置初步

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const Hello = resolve => require(['@/components/Hellow/HelloWorld'], resolve)
+const Login = resolve => require(['@/components/Login/index'], resolve)//登陆页
+const Home = resolve => require(['@/components/Home/index'], resolve)//主页
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Hello',
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
